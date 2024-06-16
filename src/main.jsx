c372c0821dbfd16f1e3f66b706361b20cdec9fb7
akan-js <akanowo@myeazipay.com>
2024-06-16 04:59:34 +0100

    fix: moved the redirect to the index.html

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-if (window.location.href.match(/netlify/i)) {
-	window.location.replace('https://dosesfest.com.ng');
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
