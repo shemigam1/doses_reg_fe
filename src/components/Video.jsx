8ea2f6b2f09167280b5a22cd622b8a584910d0cc
akan-js <akanowo@myeazipay.com>
2024-06-16 20:55:09 +0100

    fix: video not playing sound

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,8 +1,12 @@
 import ReactPlayer from 'react-player/lazy';
 function Video() {
 	return (
-		<div className='flex justify-center items-center w-[100%] h-[100%]'>
-			<ReactPlayer url="https://youtu.be/kKAf8-IPPAA" volume={null} muted width='100%' height='100%' />
+		<div className="flex justify-center items-center w-[100%] h-[100%]">
+			<ReactPlayer
+				url="https://youtu.be/kKAf8-IPPAA"
+				width="100%"
+				height="100%"
+			/>
 			{/* <video src="https://www.youtube.com/watch?v=Bs8LnMKfww0"></video> */}
 		</div>
 	);
