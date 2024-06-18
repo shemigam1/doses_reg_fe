1aeaaed93e1c8d8571fc817617dbc6cbaf0ba08d
akan-js <akanowo@myeazipay.com>
2024-06-18 18:09:45 +0100

    changed video rendering to cloudinary

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,13 +1,26 @@
 import ReactPlayer from 'react-player/lazy';
+
+function handleContextMenu(e) {
+	e.preventDefault();
+	return false;
+}
+
 function Video() {
 	return (
 		<div className="flex justify-center items-center w-[100%] h-[100%]">
-			<ReactPlayer
+			{/* <ReactPlayer
 				url="https://youtu.be/kKAf8-IPPAA"
 				width="100%"
 				height="100%"
-			/>
+			/> */}
 			{/* <video src="https://www.youtube.com/watch?v=Bs8LnMKfww0"></video> */}
+			<video controls onContextMenu={handleContextMenu}>
+				<source
+					src="https://res.cloudinary.com/dkn0vfgbg/video/upload/Doses/Doses_Main_Ad_-_Final_r9misv.mp4"
+					type="video/mp4"
+				/>
+				Your browser does not support the video tag.
+			</video>
 		</div>
 	);
 }
