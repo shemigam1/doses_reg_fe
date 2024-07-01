5a421a8b96db1c539b30d99fdd89411a74d467a7
akan-js <akanowo@myeazipay.com>
2024-07-01 10:43:05 +0100

    feat: added event closed landing page

---
+++ src/components/EventClosed/EventClosed.jsx
@@ -0,0 +1,21 @@
+import './EventClosed.css';
+
+function EventClosed() {
+	return (
+		<>
+			<div className="container">
+				<h1>Thank You for Attending!</h1>
+				<p>
+					The Doses Fest 2024 has concluded. We appreciate your participation
+					and hope you had a great time. Stay tuned for updates on future
+					events.
+				</p>
+				{/* <a href="https://www.example.com" className="button">
+				Visit Our Website
+			</a> */}
+			</div>
+		</>
+	);
+}
+
+export default EventClosed;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+// import App from './App.jsx';
 import './index.css';
+import EventClosed from './components/EventClosed/EventClosed';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		{/* <App /> */}
+		<EventClosed />
 	</React.StrictMode>
 );
